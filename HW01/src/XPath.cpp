84c6a03317ce180d825748869712dcf0eed6400c
신동환 <@SDH-PC>
2014-04-07 12:44:16 +0900

    XPath Class 생성 입력된 cmd(path)에 대한 노드를 찾고, node list를 작성하고 관리하게 할 예정.

---
+++ HW01/src/XPath.cpp
@@ -0,0 +1,18 @@
+/*
+ * XPath.cpp
+ *
+ *  Created on: 2014. 4. 7.
+ *      Author: 신동환
+ */
+
+#include "XPath.h"
+
+XPath::XPath() {
+	// TODO Auto-generated constructor stub
+
+}
+
+XPath::~XPath() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ HW01/src/XPath.h
@@ -0,0 +1,17 @@
+/*
+ * XPath.h
+ *
+ *  Created on: 2014. 4. 7.
+ *      Author: 신동환
+ */
+
+#ifndef XPATH_H_
+#define XPATH_H_
+
+class XPath {
+public:
+	XPath();
+	virtual ~XPath();
+};
+
+#endif /* XPATH_H_ */
